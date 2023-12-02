f740bdd5f84230cab1aad82cbd62edaa49c856f1
Nishantpani <151610725+Nishantpani@users.noreply.github.com>
2023-12-02 10:05:41 +0530

    Revert "social-media-app Code Updated"
    This reverts commit 1df8bbc5fd3a4f97b202c239629853da05c70d8f.

--- social-connect-app/server/src/api/auth/auth.router.js
+++
@@ -1,8 +0,0 @@
-const router = require("express").Router({ mergeParams: true });
-
-const authController = require("./auth.controller");
-
-router.post("/register", authController.register);
-router.post("/login", authController.login);
-
-module.exports = router;
--- social-connect-app/server/src/api/user/user.router.js
+++
@@ -1,7 +0,0 @@
-const router = require("express").Router({ mergeParams: true });
-
-const userController = require("./user.controller");
-
-router.get("/profile", userController.profile);
-
-module.exports = router;
